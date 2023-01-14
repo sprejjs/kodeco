bb11cc751976261eb39a61e6e9909ccc0e6664ba
Allan Spreys <allan@spreys.com>
2023-01-14 15:31:53 +0000

    Implements Flyweight example

--- Learning Paths/iOS Design Patterns/Advanced Design Patterns/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Flyweight.xcplaygroundpage/Contents.swift
+++ Learning Paths/iOS Design Patterns/Advanced Design Patterns/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Flyweight.xcplaygroundpage/Contents.swift
@@ -12,3 +12,38 @@
  ## Code Example
  */
 
+import UIKit
+
+let red = UIColor.red
+let red2 = UIColor.red
+
+print(red === red2) // true, demonstrating that UIColor is flyweight
+
+let color2 = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
+let color3 = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
+print(color2 === color3) // false
+
+extension UIColor {
+  public static var colorStore: [String: UIColor] = [:]
+
+  public class func rgba(
+    red: CGFloat,
+    green: CGFloat,
+    blue: CGFloat,
+    alpha: CGFloat
+  ) -> UIColor {
+    let key = "\(red)\(green)\(blue)\(alpha)"
+    if let color = colorStore[key] {
+      return color
+    } else {
+      let color = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+      colorStore[key] = color
+      return color
+    }
+  }
+}
+
+let flyColor = UIColor.rgba(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
+let flyColor2 = UIColor.rgba(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
+
+print(flyColor === flyColor2) // true
