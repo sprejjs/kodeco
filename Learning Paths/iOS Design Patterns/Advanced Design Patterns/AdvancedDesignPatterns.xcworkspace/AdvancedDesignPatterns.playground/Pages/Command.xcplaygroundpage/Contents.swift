08669154d98b4294e942118260556f6110fa2cb9
Allan Spreys <allan@spreys.com>
2023-01-19 11:53:37 +0000

    Adds Commander pattern example to playground

--- Learning Paths/iOS Design Patterns/Advanced Design Patterns/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Command.xcplaygroundpage/Contents.swift
+++ Learning Paths/iOS Design Patterns/Advanced Design Patterns/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Command.xcplaygroundpage/Contents.swift
@@ -13,3 +13,55 @@
  
  ## Code Example
  */
+
+import Foundation
+
+public class Door {
+  public var isOpen = false
+}
+
+public class DoorCommand {
+  public let door: Door
+
+  public init(door: Door) {
+    self.door = door
+  }
+
+  public func execute() {}
+}
+
+public class OpenDoorCommand: DoorCommand {
+  public override func execute() {
+    print("Opening the door...")
+    door.isOpen = true
+  }
+}
+
+public class CloseCommand: DoorCommand {
+  public override func execute() {
+    print("Closing the door...")
+    door.isOpen = false
+  }
+}
+
+// MAR: - Invoker
+public class Doorman {
+  public let commands: [DoorCommand]
+  public let door: Door
+
+  init(door: Door) {
+    self.commands = (0..<Int.random(in: 1...10)).map { _ in
+      return Bool.random() ? OpenDoorCommand(door: door) : CloseCommand(door: door)
+    }
+    self.door = door
+  }
+
+  public func executeCommands() {
+    print("The doorman is...")
+    commands.forEach { $0.execute() }
+  }
+}
+
+let door = Door()
+let doorman = Doorman(door: door)
+doorman.executeCommands()
