68e2a7fb94027fc750cf2f6cb4b67c42464cca6a
Allan Spreys <allan@spreys.com>
2023-02-11 11:39:04 +0000

    Includes QuadTree implementation

--- Books/02 Expert Swift/02. QuadTree/QuadTree/QuadTree.swift
+++ Books/02 Expert Swift/02. QuadTree/QuadTree/QuadTree.swift
@@ -7,26 +7,46 @@ import Foundation
 import CoreGraphics.CGBase // CGPoint
 
 struct QuadTree {
+  private var root: Node
   private(set) var count = 0
 
   init(region: CGRect) {
+    root = Node(region: region)
   }
 
   @discardableResult
   mutating func insert(_ point: CGPoint) -> Bool {
-    false
+    if !isKnownUniquelyReferenced(&root) {
+      root = root.copy()
+    }
+    if root.insert(point) {
+      count += 1
+      return true
+    }
+    return false
   }
 
   func find(in searchRegion: CGRect) -> [CGPoint] {
-    []
+    root.find(in: searchRegion)
   }
 
   func points() -> [CGPoint] {
-    []
+    find(in: root.region)
+  }
+
+  private func collectRegions(_ node: Node, into regions: inout [CGRect]) {
+    regions.append(node.region)
+    if let quad = node.quad {
+      for child in quad.all {
+        collectRegions(child, into: &regions)
+      }
+    }
   }
 
   func regions() -> [CGRect] {
-    []
+    var regions: [CGRect] = []
+    collectRegions(root, into: &regions)
+    return regions
   }
 }
 
@@ -36,7 +56,7 @@ private final class Node {
   var points: [CGPoint] = []
   var quad: Quad?
 
-  init(region: CGRect, points: CGPoint = [], quad: Quad? = nil) {
+  init(region: CGRect, points: [CGPoint] = [], quad: Quad? = nil) {
     self.region = region
     self.quad = quad
     self.points = points
@@ -44,7 +64,70 @@ private final class Node {
     precondition(points.count <= maxItemCapacity)
   }
 
-  struct Quad {
+  func copy() -> Node {
+    Node(region: region, points: points, quad: quad?.copy())
+  }
 
+  func subdivide() {
+    precondition(quad == nil, "Already subdivided")
+    quad = Quad(region: region)
+  }
+
+  func find(in searchRegion: CGRect) -> [CGPoint] {
+    guard region.intersects(searchRegion) else {
+      return []
+    }
+    var result = points.filter { searchRegion.contains($0) }
+    if let quad = quad {
+      result += quad.all.flatMap { $0.find(in: searchRegion) }
+    }
+    return result
+  }
+
+  @discardableResult
+  func insert(_ point: CGPoint) -> Bool {
+    if !region.contains(point) {
+      return false
+    }
+    if points.count < maxItemCapacity {
+      points.append(point)
+      return true
+    }
+    if quad == nil {
+      subdivide()
+    }
+    return quad!.all.contains { $0.insert(point) }
+  }
+
+  struct Quad {
+    let topLeft: Node
+    let topRight: Node
+    let bottomLeft: Node
+    let bottomRight: Node
+
+    var all: [Node] {
+      [topLeft, topRight, bottomLeft, bottomRight]
+    }
+
+    init(region: CGRect) {
+      let (top, bottom) = region.divided(atDistance: region.height / 2, from: .minYEdge)
+      let (topLeft, topRight) = top.divided(atDistance: region.width / 2, from: .minXEdge)
+      let (bottomLeft, bottomRight) = bottom.divided(atDistance: region.width / 2, from: .minXEdge)
+      self.topLeft = Node(region: topLeft)
+      self.topRight = Node(region: topRight)
+      self.bottomLeft = Node(region: bottomLeft)
+      self.bottomRight = Node(region: bottomRight)
+    }
+
+    private init(topLeft: Node, topRight: Node, bottomLeft: Node, bottomRight: Node) {
+      self.topLeft = topLeft
+      self.topRight = topRight
+      self.bottomLeft = bottomLeft
+      self.bottomRight = bottomRight
+    }
+
+    func copy() -> Quad {
+      Quad(topLeft: topLeft, topRight: topRight, bottomLeft: bottomLeft, bottomRight: bottomRight)
+    }
   }
 }
