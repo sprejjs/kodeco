595632ad897b2a3126b070f2452e17ab9b5daf99
Allan Spreys <allan@spreys.com>
2023-02-16 06:33:41 +0000

    More sequence examples

--- Books/02 Expert Swift/06. Countdown.playground/Contents.swift
+++ Books/02 Expert Swift/06. Countdown.playground/Contents.swift
@@ -43,18 +43,17 @@ for value in stride(from: 5, through: 0, by: -1) {
 }
 
 print("----")
-let sequenceA = sequence(first: 5) { value in value > 0 ? value - 1 : nil }
+let sequenceA = sequence(first: 5) { value in
+  value > 0 ? value - 1 : nil
+}
 sequenceA.forEach {
   print($0) // 5, 4, 3, 2, 1, 0
 }
-print("----")
-sequenceA.forEach {
-  print($0) // 5, 4, 3, 2, 1, 0
-}
+
 print("----")
 let sequenceB = sequence(state: 5) { state in
-  if state > 0 {
-    state -= 1
+  if state >= 0 {
+    defer { state -= 1 }
     return state
   } else {
     return nil
@@ -63,7 +62,24 @@ let sequenceB = sequence(state: 5) { state in
 sequenceB.forEach {
   print($0) // 5, 4, 3, 2, 1, 0
 }
+
+extension Sequence {
+  func eraseToAnySequence() -> AnySequence<Element> {
+    AnySequence(self)
+  }
+}
+
+let erased: AnySequence = sequenceB.eraseToAnySequence()
+
 print("----")
-sequenceB.forEach {
-  print($0) // no output
+let anotherCountdownFrom5 = AnySequence {
+  var count = 5
+
+  return AnyIterator<Int> {
+    defer { count -= 1}
+    return count >= 0 ? count : nil
+  }
+}
+anotherCountdownFrom5.forEach {
+  print($0)
 }
