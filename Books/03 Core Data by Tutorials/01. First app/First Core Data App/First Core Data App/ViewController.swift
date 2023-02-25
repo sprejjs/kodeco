8c5ab1d66927634eff241fedf0612f6022f2edf6
Allan Spreys <allan@spreys.com>
2023-02-25 20:42:55 +0000

    Completes the first version of the database

--- Books/03 Core Data by Tutorials/01. First app/First Core Data App/First Core Data App/ViewController.swift
+++ Books/03 Core Data by Tutorials/01. First app/First Core Data App/First Core Data App/ViewController.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
   @IBOutlet weak var tableView: UITableView!
-  var names: [String] = []
+  private var people: [NSManagedObject] = []
 
   override func viewDidLoad() {
     super.viewDidLoad()
     setupView()
+    fetchPeopleFromDatabase()
   }
 
   private func setupView() {
@@ -27,7 +29,7 @@ class ViewController: UIViewController {
 
     let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] _ in
       guard let textField = alert.textFields?.first, let nameToSave = textField.text else { return }
-      self.names.append(nameToSave)
+      saveNewName(nameToSave)
       self.tableView.reloadData()
     }
 
@@ -38,11 +40,51 @@ class ViewController: UIViewController {
 
     present(alert, animated: true)
   }
+
+  private func fetchPeopleFromDatabase() {
+    // This is the simplest version of the fetch request, it fetches all objects
+    let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+
+    do {
+      // Here we execute the fetch request
+      people = try managedContext.fetch(fetchRequest)
+    } catch let error as NSError {
+      print("Could not fetch. \(error), \(error.userInfo)")
+    }
+  }
+  
+  private func saveNewName(_ name: String) {
+    // Here we are creating a new entity matching the name specified in the data model
+    guard let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext) else { return }
+    // Here we create a new managed object and insert it into the managed context
+    let person = NSManagedObject(entity: entity, insertInto: managedContext)
+    // We can create multiple managed objects from the same entity
+    let person2 = NSManagedObject(entity: entity, insertInto: managedContext)
+
+    // Setting value with KVC
+    person.setValue(name, forKeyPath: "name")
+    person2.setValue("\(name) 2", forKey: "name")
+
+    // Saving the context saved all of the objects in the graph
+    appDelegate.saveContext()
+    people.append(person)
+  }
+
+  private lazy var appDelegate: AppDelegate = {
+    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+      fatalError("Unable to get app delegate")
+    }
+    return appDelegate
+  }()
+
+  private lazy var managedContext: NSManagedObjectContext = {
+    appDelegate.persistentContainer.viewContext
+  }()
 }
 
 extension ViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    names.count
+    people.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -50,7 +92,8 @@ extension ViewController: UITableViewDataSource {
       fatalError("Unable to dequeue a cell")
     }
 
-    cell.textLabel?.text = names[indexPath.row]
+    let person = people[indexPath.row]
+    cell.textLabel?.text = person.value(forKeyPath: "name") as? String
 
     return cell
   }
